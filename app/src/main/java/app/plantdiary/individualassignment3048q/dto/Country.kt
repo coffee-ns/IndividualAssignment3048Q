52385623d82ebb67f6cba0e738a1be035cbc3baf
coffee-ns <coffeens@mail.uc.edu>
2021-02-28 14:23:11 -0500

    Had name & code flipped around in toString override. Changed to satisfy ToString unit test.

--- app/src/main/java/app/plantdiary/individualassignment3048q/dto/Country.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/dto/Country.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class Country (@SerializedName("Code") var code : String, @SerializedName("Name") var name : String) {
 
     override fun toString(): String {
-        return "$code, $name"
+        return "$name $code"
     }
 
 }
